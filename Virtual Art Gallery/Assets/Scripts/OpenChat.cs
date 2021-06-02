106be7edce6de4008f343597dd2c12ddd881ec87
ppapaioannou <ppapaioannou13@gmail.com>
2021-06-02 17:26:11 +0300

    updated museum

---
+++ Virtual Art Gallery/Assets/Scripts/OpenChat.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenChat : MonoBehaviour
+{
+    [SerializeField] GameObject Chat;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            //if (Chat != null)
+            //{
+            bool isActivated = Chat.activeSelf;
+            Chat.SetActive(!isActivated);
+            //Chat.SetActive(true);
+            //}
+        }
+    }
+}
